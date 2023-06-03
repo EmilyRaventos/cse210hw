d48513153bd3fe76811fbbf5a30b66a244461850
Emily Raventos <emilyraventos@gmail.com>
2023-06-02 18:48:33 -0600

    updates

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // Test Step 3
+        Assignment a1 = new Assignment("Emily Raventos", "Inheritance");
+        Console.WriteLine(a1.GetSummary());
+
+        // Test Step 4
+        MathAssignment ma1 = new MathAssignment("Joshua Raventos", "Fractions", "5.2", "1-10");
+        Console.WriteLine(ma1.GetSummary());
+        Console.WriteLine(ma1.GetHomeworkList());
+
+        // Test Step 5
+        WritingAssignment wa1 = new WritingAssignment("Kylee Raventos", "Creative Writing", "The Hourglass Chronicles");
+        Console.WriteLine(wa1.GetSummary());
+        Console.WriteLine(wa1.GetWritingInformation());
     }
 }
