1d818e6a1292091ad2447fac6925f78d268c6749
Emily Raventos <emilyraventos@gmail.com>
2023-07-13 13:57:12 -0600

    Try to get foundation 4 github to work

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,9 +1,24 @@
 using System;
 
-class Program
+public class Program
 {
+    static List<Activity> _listOfActivities;
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        List<Activity> listOfActivities = new List<Activity>();
+        _listOfActivities = listOfActivities;
+
+        Biking a1 = new Biking("03 November 2022", 30, 8);
+        Running a2 = new Running("12 January 2023", 20, 3);
+        Swimming a3 = new Swimming("24 April 2023", 60, 30);
+
+        _listOfActivities.Add(a1);
+        _listOfActivities.Add(a2);
+        _listOfActivities.Add(a3);
+
+        foreach (Activity activity in _listOfActivities)
+        {
+            activity.GetSummary(activity);
+        }
     }
 }
--- final/Foundation4/running.cs
+++ final/Foundation4/running.cs
@@ -2,5 +2,28 @@ using System;
 
 public class Running : Activity
 {
-    
+    private double _distance;
+
+    public Running (string date, int length, double distance)
+    {
+        _date = date;
+        _length = length;
+        _distance = distance;
+        _type = "Running";
+    }
+
+    public override double GetSpeed()
+    {
+        return (_distance / _length) * 60;
+    }
+
+    public override double GetPace()
+    {
+        return _length / _distance;
+    }
+
+    public override double GetDistance()
+    {
+        return _distance;
+    }
 }
