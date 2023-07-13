94bbbe4fb7f60ec003c426e81e7601dcbdc21b06
Emily Raventos <emilyraventos@gmail.com>
2023-07-13 15:38:19 -0600

    Add a comment

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -9,10 +9,10 @@ class Program
         List<Video> listOfVideos = new List<Video>();
         _listOfVideos = listOfVideos;
 
-        Video v1 = new Video("Dune", "Denis Villeneuve", 9300);
-        Video v2 = new Video("Avatar", "James Cameron", 9720);
-        Video v3 = new Video("Pride and Prejudice", "Joe Wright", 7740);
-        Video v4 = new Video("Captain America: The First Avenger", "Joe Johnston", 7440);
+        Video v1 = new Video("Dune Trailer", "Denis Villeneuve", 208);
+        Video v2 = new Video("Avatar Trailer", "James Cameron", 149);
+        Video v3 = new Video("Pride and Prejudice Trailer", "Joe Wright", 161);
+        Video v4 = new Video("Captain America: The First Avenger Trailer", "Joe Johnston", 145);
 
         Comment c1 = new Comment("Jane Doe", "I did not read the book and I did not know what to expect exactly. I LOVED it.");
         Comment c2 = new Comment("Johnny Smith", "This is an all-time great science fiction movie");
@@ -31,16 +31,18 @@ class Program
         Comment c7 = new Comment("Edward Stevenson", "A timeless adaptation of a timeless Jane Austen novel.");
         Comment c8 = new Comment("Sammi Hansen", "Wonderful Moments, Both Big and Small");
         Comment c9 = new Comment("Eliza Wood", "There's really nothing negative to say about the film. The actors are perfectly cast, the direction is subtle and well-realized, and even a shade more cinematic than I would have expected, or than it needed to be.");
+        Comment c10 = new Comment("Chris Freeman", "This looks so good!");
         v3.AddComment(c7);
         v3.AddComment(c8);
         v3.AddComment(c9);
+        v3.AddComment(c10);
 
-        Comment c10 = new Comment("Kristen Thomas", "It's a good, action packed film.");
-        Comment c11 = new Comment("Nick Random", "The story itself was fine, and I'm personally a big fan of Captain America's origin story so this movie was a nice watch for me. Maybe not so much if you don't care at all about superheroes.");
-        Comment c12 = new Comment("Lindsey Macey", "A weak man knows the value of strength.");
-        v4.AddComment(c10);
+        Comment c11 = new Comment("Kristen Thomas", "It's a good, action packed film.");
+        Comment c12 = new Comment("Nick Random", "The story itself was fine, and I'm personally a big fan of Captain America's origin story so this movie was a nice watch for me. Maybe not so much if you don't care at all about superheroes.");
+        Comment c13 = new Comment("Lindsey Macey", "A weak man knows the value of strength.");
         v4.AddComment(c11);
         v4.AddComment(c12);
+        v4.AddComment(c13);
 
         _listOfVideos.Add(v1);
         _listOfVideos.Add(v2);
@@ -60,7 +62,6 @@ class Program
     {
         foreach (Video video in _listOfVideos)
         {
-            Index(video)
             video.DisplayInformation();
         }
     }
--- final/Foundation1/comment.cs
+++ final/Foundation1/comment.cs
@@ -12,6 +12,6 @@ public class Comment
     }
     public void DisplayComment()
     {
-        Console.WriteLine($" - {_name} - {_comment}");
+        Console.WriteLine($" - {_name}: {_comment}");
     }
 }
